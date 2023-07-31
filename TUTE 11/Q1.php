81426537d6909ef708bb78a13d3c2fad6ff80d46
suthanthirany <136632314+suthanthirany@users.noreply.github.com>
2023-07-31 20:56:01 +0530

    Add files via upload

---
+++ TUTE 11/Q1.php
@@ -0,0 +1,13 @@
+
+   <?php
+   $var=X;
+   $var=Y;
+
+   $Sum=X+Y;
+   $var Difference=X-Y;
+   $var Product=X*Y;
+   $var Division=X/Y;
+
+   echo("Sum : " +$Sum);
+   ?>
+    
---
+++ TUTE 11/Q5.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("Apple", "Banana", "Orange", "Mango", "Grapes");
+foreach ($fruits as $fruit) {
+  echo $fruit . "<br>";
+}
+?>
